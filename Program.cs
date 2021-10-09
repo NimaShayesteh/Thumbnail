79ac4476046f830b4d567f2e572992b1781b92c2
Nima Shayesteh <nima.tyson@gmail.com>
2021-10-09 18:04:17 +0330

    latest

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace imageThumbnail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = @"c:\deletekon\1.png";
+            Image image = Image.FromFile(filename);
+            Image thumb = image.GetThumbnailImage(image.Width, image.Height, () => false, IntPtr.Zero);
+            thumb.Save(Path.ChangeExtension(filename, "thumb"));
+
+        }
+    }
+}
